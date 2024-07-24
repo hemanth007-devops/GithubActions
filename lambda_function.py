b08abba66c3bd3e9f87a66355371fc94db2df413
hemanth007-devops <69528581+hemanth007-devops@users.noreply.github.com>
2024-07-24 19:41:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,19 +2,19 @@ import boto3
 
 def lambda_handler(event, context):
     glue = boto3.client('glue')
-    job_name = 'your_glue_job_name'  # Replace with your actual Glue job name
+    workflow_name = 'your_glue_workflow_name'  # Replace with your actual Glue workflow name
 
     try:
-        response = glue.start_job_run(JobName=job_name)
-        job_run_id = response['JobRunId']
-        print(f"Started Glue job run with ID: {job_run_id}")
+        response = glue.start_workflow_run(Name=workflow_name)
+        workflow_run_id = response['RunId']
+        print(f"Started Glue workflow run with ID: {workflow_run_id}")
         return {
             'statusCode': 200,
-            'body': f"Started Glue job run with ID: {job_run_id}"
+            'body': f"Started Glue workflow run with ID: {workflow_run_id}"
         }
     except Exception as e:
-        print(f"Error starting Glue job: {str(e)}")
+        print(f"Error starting Glue workflow: {str(e)}")
         return {
             'statusCode': 500,
-            'body': f"Error starting Glue job: {str(e)}"
+            'body': f"Error starting Glue workflow: {str(e)}"
         }
